130240999ef1594023fa2f3e245556a1e36600d7
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2024-06-04 13:47:04 +0200

    feat: add doc and example

---
+++ app/Console/Commands/LaravelTraining.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class LaravelTraining extends Command
+{
+    protected $signature = 'run:functionality_tests';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
